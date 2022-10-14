c2ef28c82707c8c89023db9875ede1305071e22b
DavidMaglica <davidmaglica@protonmail.ch>
2022-10-15 00:29:34 +0200

    Izmjena na zadatku 1

--- zadatak1.py
+++ zadatak1.py
@@ -3,6 +3,6 @@ def fn(x):
     Funkcija uzima listu string-ova. Provjeri dal su sve stringovi, ako ne error.
     Vraca novu listu, gdje su string-ovi duži od 4 znaka.
     """
-    return([item  for item in x if len(item) >4 and isinstance(item, str) == True])    
+    return([item  for item in x if len(item) > 4 and isinstance(item, str) == True])    
     
 print(fn(["Pas", "Macka", "Stol"]))
